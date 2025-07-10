83e00b44c7c94ea8503238b41fe5b6eb31328d97
romanyrefaat1 <refaatromany641@gmail.com>
2025-07-10 16:00:26 +0300

    Fix caching

--- components/embeds/embed-toast.tsx
+++ components/embeds/embed-toast.tsx
@@ -71,7 +71,7 @@ export default function EmbedToast() {
   // if (jwtToken) {
     return (
       <Script
-        src="https://cdn.jsdelivr.net/gh/romanyrefaat1/hooklify-widgets/widgets/toast/toast.js"
+        src="https://cdn.jsdelivr.net/gh/romanyrefaat1/hooklify-widgets@7a5a7af248244b8a2dfd2a6be08bc1284332f68f/widgets/toast/toast.js"
         strategy="afterInteractive"
         // data-jwt-token={jwtToken}
         data-site-id="site_0c812a1f-4662-4017-a841-e7475c796c3c"
