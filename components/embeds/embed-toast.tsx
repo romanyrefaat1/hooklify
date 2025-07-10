42319a4c563deb6cd0415864f42ea8f63bd8b83f
romanyrefaat1 <refaatromany641@gmail.com>
2025-07-10 16:03:35 +0300

    Better cache handling

--- components/embeds/embed-toast.tsx
+++ components/embeds/embed-toast.tsx
@@ -71,9 +71,8 @@ export default function EmbedToast() {
   // if (jwtToken) {
     return (
       <Script
-        src="https://cdn.jsdelivr.net/gh/romanyrefaat1/hooklify-widgets@7a5a7af248244b8a2dfd2a6be08bc1284332f68f/widgets/toast/toast.js"
+        src="https://cdn.jsdelivr.net/gh/romanyrefaat1/hooklify-widgets/widgets/toast/toast.js?v=2" // ?v=2 to force cache refresh
         strategy="afterInteractive"
-        // data-jwt-token={jwtToken}
         data-site-id="site_0c812a1f-4662-4017-a841-e7475c796c3c"
         data-widget-id="widget_d0673215-9e3e-4ef1-93fe-6eeaa42be95b"
         onLoad={() => {
